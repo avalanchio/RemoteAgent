90a409e08a7a16101e0bddebb7df3263860f5bd6
Abul Basar <abulbasar@gmail.com>
2025-02-21 15:54:04 +0530

    Minor updates to remote agent

--- remote_agent.py
+++ remote_agent.py
@@ -2,18 +2,20 @@ import http.client
 import inspect
 import json
 import os
+import platform
 import shlex
 import shutil
 import ssl
 import subprocess
 import sys
 import urllib.parse
-from time import time, sleep
+from time import time, sleep, tzname
+import socket
 import pathlib
 import configparser
-import multiprocessing as mp
+import threading
 
-"""
+help_text = """
 This is simple avalanchio agent. 
 It must always be dependent on the core libraries only without any external dependencies.
 It polls task queue and executes it. 
@@ -21,16 +23,50 @@ It polls task queue and executes it.
 topic: Optional field to pull data from a specific queue. 
        Valid topic: user, rule. Set the topic to rule to enable rule playbook execution
        If you do not specify any topic, any type of task would be executed
-       You can run multiple remote_agent for task parallelism 
+       You can run multiple remote_agent for task parallelism
+
+---------------------------------------------------------------------------------
+Property                  | Description                                         |
+---------------------------------------------------------------------------------
+token                     | Avalanchio Jwot token. Agent will use this token    |
+                            for communication with avalanchio servers. If you 
+                            do not set this value and base_url, the script will
+                            try to check if environment variable (AIO_TOKEN)
+                            and $HOME/aio/avalanchio.ini file for token as fall
+                            back mechanism.  
+--------------------------|-----------------------------------------------------|
+base_url                  | Avalanchio endpoint, e.g. https://api.avalanchio.com
+                            Like the token, if the value is not provided, the 
+                            script searches the environment variable 
+                            (AIO_BASE_URL) and $HOME/aio/avalanchio.ini. 
+--------------------------|-----------------------------------------------------|
+topic                     | [Optional] topic to subscribe to, e.g. user, user/1,|
+                            rule, rule/1 etc. Essentially it is scan prefix to 
+                            find the next task in the queue. 
+--------------------------|-----------------------------------------------------|
+disable_ssl_verification  | Disable SSL verification during the http client     |
+                            communication
+--------------------------|-----------------------------------------------------|
+num_workers               | Maximum number of process workers to run at a given |
+                            time. Default: 1
+--------------------------|-----------------------------------------------------|
+task_timeout_second       | [Optional] Timeout for the tasks. Set it to None    |
+                            to disable the timeout check. Default: None
+--------------------------|-----------------------------------------------------|
+remote_agent_id           | [Optional] Id of the remote agent. Used for         |
+                            tracking or blocking from the server side. Default
+                            value is the host name. Default: <hostname>
+--------------------------|-----------------------------------------------------|
 """
 ############## Update the settings in section based on your requirement ##########
 token = None
 base_url = None
 topic = None
 disable_ssl_verification = True
-delete_working_directory = False
+delete_working_directory = True
 num_workers = 4
 task_timeout_second = None
+remote_agent_id = None
 ###################################################################################
 
 current_directory = os.getcwd()
@@ -38,6 +74,11 @@ tmp_directory = os.getenv('AIO_TMP_DIRECTORY', os.path.join(current_directory, "
 
 
 def connection_info(profile = None):
+    """
+    This function is used to read base_url and token
+    from ~/aio/avalanchio.ini file if they are not provided
+    in the above global variables.
+    """
     token_ = os.getenv("AIO_TOKEN")
     base_url_ = os.getenv("AIO_BASE_URL")
     ini_file = os.path.join(pathlib.Path.home(), "aio", "avalanchio.ini")
@@ -60,6 +101,15 @@ def to_str(s):
         return s.decode()
     return str(s)
 
+
+def agent_info():
+    return {
+        "os": platform.system(),
+        "os_version": platform.release(),
+        "timezone": str(tzname[0]),
+        "name": socket.gethostname(),
+    }
+
 def execute_cmd(cmd, timeout = None, env = None):
     if isinstance(cmd, str):
         cmd = shlex.split(cmd)
@@ -78,7 +128,7 @@ def execute_cmd(cmd, timeout = None, env = None):
         response["message"] = str(e)
     return response
 
-def execute_python_script(working_directory:str, input_data:dict, code:str):
+def execute_python_script(working_directory:str, input_data:dict, code:str, timeout:int = None):
 
     input_s = json.dumps(input_data)
 
@@ -123,7 +173,7 @@ def execute_python_script(working_directory:str, input_data:dict, code:str):
     executable = os.getenv("PYTHON_EXECUTABLE", sys.executable)
     cmd = [executable, executor_file]
     try:
-        res = execute_cmd(cmd)
+        res = execute_cmd(cmd, timeout=timeout)
         if os.path.isfile(output_file):
             with open(output_file) as f:
                 res['response'] = f.read()
@@ -166,16 +216,17 @@ def execute_task(task_data:dict):
     elif task_type == 'python':
         # Execute Python script
         working_directory = os.path.join(tmp_directory, str(task_id))
-        pid = os.getpid()
-        print(f"Executing python task: {task_id}, working directory: {working_directory}, pid: {pid}")
+        print(f"Executing python task: {task_id}, working directory: {working_directory}")
         os.makedirs(working_directory)
         try:
             params = task_data.get('data')
             if params is None:
                 params = {}
+            timeout = task_data.get("timeout", task_timeout_second)
             result = execute_python_script(working_directory=working_directory,
                                            input_data=params,
-                                           code=code)
+                                           code=code,
+                                           timeout=timeout)
             return result
         finally:
             # Optional: Clean up output directory
@@ -207,6 +258,8 @@ class BackgroundTaskProcessor:
         self.parse_url(server_url)
         self.running_processes = list()
         self.last_poll_ts = 0.0
+        self.stop = threading.Event()
+        self.shared_agent_info = False
 
     def parse_url(self, url):
         """
@@ -232,8 +285,9 @@ class BackgroundTaskProcessor:
     def make_conn(self):
         # Choose connection based on URL
         if self.scheme == 'https':
-            ctx = ssl.create_default_context()
+            ctx = None
             if disable_ssl_verification:
+                ctx = ssl.create_default_context()
                 ctx.check_hostname = False
                 ctx.verify_mode = ssl.CERT_NONE
             conn = http.client.HTTPSConnection(self.host, context=ctx)
@@ -242,7 +296,7 @@ class BackgroundTaskProcessor:
         return conn
 
     def _make_request_raw(self, method:str, path:str, params:dict = None, body:dict = None)->dict:
-        self.conn = self.make_conn()
+        conn = self.make_conn()
         try:
             headers = {'Accept': 'application/json',
                        'Content-Type': 'application/json',
@@ -253,29 +307,40 @@ class BackgroundTaskProcessor:
             if isinstance(body, dict):
                 body = json.dumps(body)
             assert body is None or isinstance(body, str)
-            self.conn.request(method, path, headers=headers, body = body)
-            response = self.conn.getresponse()
+            conn.request(method, path, headers=headers, body = body)
+            response = conn.getresponse()
             # Check successful response
             if response.status == 200:
                 content = response.read()
                 if content:
                     return json.loads(content.decode())
             else:
-                raise RuntimeError(str(response.status) + " " + str(response.read()))
+                content = response.read().decode()
+                raise RuntimeError(str(response.status) + " " + content)
         finally:
-            self.conn.close()
+            conn.close()
 
-    def find_task(self, topic:str = None)->dict:
+    def find_tasks(self, topic:str = None, size: int = 1):
         """
         Make HTTP request to server to fetch tasks.
 
         :return: Parsed JSON response or None
         """
+        self.last_poll_ts = time()
+        running_task_ids = [str(r[0].get('id')) for r in self.running_processes]
+        params = dict()
+        if topic:
+            params['topic'] = topic
+        params['size'] = size
+        params['agent-id'] = remote_agent_id
+        if running_task_ids:
+            params['running'] = "|".join(running_task_ids)
+        if not self.shared_agent_info:
+            params['agent-info'] = json.dumps(agent_info())
+            self.shared_agent_info = True
         path = "/api/v1/remote-agent/poll"
+        # print("Polling for new tasks")
         for attempt in range(self.max_retries):
-            params = dict()
-            if topic:
-                params['topic'] = topic
             try:
                 return self._make_request_raw('GET', path, params)
             except Exception as e:
@@ -283,9 +348,18 @@ class BackgroundTaskProcessor:
                 sleep(5)  # Wait before retry
 
     def acknowledge_task(self, task_id:int):
+        """
+        Before processing the task, send an ack to the server
+        The server changes the status of the task from Queue to Processing
+        """
         return self._make_request_raw("PATCH", f"/api/v1/remote-agent/{task_id}/ack")
 
     def save_result(self, task_id:int, result:dict):
+        """
+        Send the task output upon completion.
+        Upon received this request, the server marks the task as Complete
+        and delete from the queue (default).
+        """
         return self._make_request_raw("POST", f"/api/v1/remote-agent/{task_id}/response", body = result)
 
     # def is_task_stopped(self, task_id):
@@ -309,44 +383,47 @@ class BackgroundTaskProcessor:
         duration = time()-start_time
         print(f"Completed task {task_id}, duration: {duration:.2f}sec")
 
+
     def run(self):
         """
         Main processing loop to continuously check for tasks.
         """
-        pid = os.getpid()
-        print(f"Background Task Processor started. Base URL: {self.server_url}, process id: {pid}")
-        print(f"Watching for tasks in queue: {self.topic}")
-        print(f"Working directory: {tmp_directory}")
-        while True:
+        print(f"Background Task Processor started. Base URL: {self.server_url}, "
+              f"working directory: {tmp_directory}, "
+              f"num_workers: {num_workers}, "
+              f"remote_agent_id: {remote_agent_id}")
+        os.makedirs(tmp_directory, exist_ok=True)
+        while not self.stop.is_set():
             try:
-                for p in self.running_processes:
-                    if isinstance(task_timeout_second, float) and time()-p[2]>task_timeout_second:
-                        p[1].kill()
-                    # if not p[1].is_alive():
-                    #     p[1].close()
                 self.running_processes = [r for r in self.running_processes if r[1].is_alive()]
                 if len(self.running_processes) >= num_workers:
                     sleep(0.1)
                     continue
-                task = self.find_task(self.topic)
-                if task:
-                    self.last_task_time = time()
-                    proc = mp.Process(target=self.loop_task, args=(task,))
-                    proc.start()
-                    self.running_processes.append((task, proc, time()))
-                else:
-                    sleep(self.poll_interval)
+                tasks = self.find_tasks(self.topic, num_workers - len(self.running_processes))
+                if isinstance(tasks, list) and tasks:
+                    for task in tasks:
+                        proc = threading.Thread(target=self.loop_task, args=(task,))
+                        self.running_processes.append((task, proc, time()))
+                        proc.start()
+                elif time()-self.last_poll_ts < 1.0:
+                    sleep(1.0)
             except KeyboardInterrupt:
-                print("\nTask processor stopped by user.")
-                break
+                self.stop.set()
             except Exception as e:
                 print(f"Unexpected error: {e}")
+                sleep(3.0)
+        print("Exiting agent")
 
 
 def main():
-    global token, base_url, topic
-    if not(token and base_url):
-        token, base_url = connection_info()
+    global token, base_url, topic, remote_agent_id
+    token_, base_url_ = connection_info()
+    if not token:
+        token = token_
+    if not base_url:
+        base_url = base_url_
+    if remote_agent_id is None:
+        remote_agent_id = socket.gethostname()
     poll_interval = float(os.getenv("AGENT_POLL_INTERVAL", "1.0"))
     max_retries = int(os.getenv("AGENT_MAX_RETRIES", "100"))
     if not token:
